209f0815cdb355ec97620164b123d8f5c66690fd
Alvaro S S T Santos <alvarosstsantos@gmail.com>
2021-03-16 11:33:36 -0300

    added user-record-api

---
+++ user-record-api/src/modules/users/useCases/createUser/CreateUserUseCase.ts
@@ -0,0 +1,25 @@
+import { User } from "../../model/User";
+import { IUsersRepository } from "../../repositories/IUsersRepository";
+
+interface IRequest {
+  name: string;
+  email: string;
+}
+
+class CreateUserUseCase {
+  constructor(private usersRepository: IUsersRepository) {}
+
+  execute({ email, name }: IRequest): User {
+    const emailAlreadyTaken = this.usersRepository.findByEmail(email);
+
+    if (emailAlreadyTaken) {
+      throw new Error("email already taken");
+    }
+
+    const user = this.usersRepository.create({ name, email });
+
+    return user;
+  }
+}
+
+export { CreateUserUseCase };
