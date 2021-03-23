3085c56e944e223896bb5d03c8c939319bc06e09
Alvaro S S T Santos <alvarosstsantos@gmail.com>
2021-03-23 16:40:29 -0300

    added queries-challenge

---
+++ queries-challenge/src/modules/users/repositories/implementations/UsersRepository.ts
@@ -0,0 +1,38 @@
+import { getRepository, Repository } from 'typeorm';
+
+import { IFindUserWithGamesDTO, IFindUserByFullNameDTO } from '../../dtos';
+import { User } from '../../entities/User';
+import { IUsersRepository } from '../IUsersRepository';
+
+export class UsersRepository implements IUsersRepository {
+  private repository: Repository<User>;
+
+  constructor() {
+    this.repository = getRepository(User);
+  }
+
+  async findUserWithGamesById({
+    user_id,
+  }: IFindUserWithGamesDTO): Promise<User> {
+    const user = await this.repository.findOne({id: user_id}, {relations:['games']} )
+    if(!user){
+      throw new Error('user does not exists')
+    }
+    return user
+  }
+
+  async findAllUsersOrderedByFirstName(): Promise<User[]> {
+    return this.repository.query(`SELECT * FROM users ORDER BY first_name ASC`); // Complete usando raw query
+  }
+
+  async findUserByFullName({
+    first_name,
+    last_name,
+  }: IFindUserByFullNameDTO): Promise<User[] | undefined> {
+    return this.repository.query(`SELECT * FROM users
+                                  WHERE
+                                  first_name ILIKE '${first_name}' AND
+                                  last_name ILIKE '${last_name}'
+    `); // Complete usando raw query
+  }
+}
